92a35912ddb6c15998cebf864bab933021d38a70
Nadav <nadav1410@gmail.com>
2021-05-04 13:51:29 +0300

    added services

---
+++ services/storage-service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+export const storageService = {
+    loadFromStorage,
+    saveToStorage
+}
+
+function loadFromStorage(key) {
+    var str = localStorage.getItem(key)
+    var val = JSON.parse(str);
+    return val;
+}
+
+function saveToStorage(key, val) {
+    var str = JSON.stringify(val)
+    localStorage.setItem(key, str)
+}
