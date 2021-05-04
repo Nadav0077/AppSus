52f70af92c489998706a4ddac101a020dad73ef1
Nadav <nadav1410@gmail.com>
2021-05-04 22:55:43 +0300

    added services

--- Apps/Keep/cmps/AddNote.jsx
+++ Apps/Keep/cmps/AddNote.jsx
@@ -9,41 +9,57 @@ export class AddNote extends React.Component {
     }
 
     state = {
-        inputVal:null
+        inputVal: null,
+        type: 'NoteTxt'
     }
 
     addTextInput = () => {
+        console.log('test')
         return <form onSubmit={(ev) => {
             ev.preventDefault();
-            this.onAddTextNote(ev)
+            this.onAddTextNote()
         }}> <input type="text" onInput={this.handleChange} /><button>+</button></form>
     }
 
     onAddTextNote = () => {
-        console.log(this.state.inputVal)
-        keepService.addNote('NoteText',{txt:this.state.inputVal,style:{backgroundColor:'#C8B6FF'}}).then(()=>{
+        var info = ''
+
+        switch (this.state.type) {
+            case 'NoteText': info = { txt: this.state.inputVal, style: { backgroundColor: '#C8B6FF' } }
+                break;
+            case 'NoteImg': info = { url: this.state.inputVal, title: 'My image', style: { backgroundColor: '#C8B6FF' } }
+                break;
+            case 'NoteTodos': {
+                console.log(this.state.inputVal.split(','))
+            //    const todos = this.state.inputVal.split(',').map(todo=>{return{txt:todo, doneAt:null}})
+                info = { label: 'New Todo', todos, style: { backgroundColor: '#C8B6FF' } }
+            }
+                break;
+
+        }
+        keepService.addNote(this.state.type, info).then(() => {
             this.props.onRenderPage()
         })
     }
 
     handleChange = ({ target }) => {
         const value = target.value
-        this.setState({inputVal:value})
+        this.setState({ inputVal: value })
     }
     render() {
         console.log(this.props)
         return (
             <div className="add-note-container">
                 <nav>
-                    <NavLink to={`/note/addTextNote`}>Add Text</NavLink>
-                    <NavLink to={`/note/addImgNote`}>Add Img</NavLink>
-                    <NavLink to={`/note/addTodoNote`}>Add Todo</NavLink>
+                    <NavLink to={`/note/addTextNote`}><span onClick={() => { this.setState({ type: 'NoteText' }) }}>Add Text</span></NavLink>
+                    <NavLink to={`/note/addImgNote`}><span onClick={() => { this.setState({ type: 'NoteImg' }) }}>Add Img</span></NavLink>
+                    <NavLink to={`/note/addTodoNote`}><span onClick={() => { this.setState({ type: 'NoteTodos' }) }}>Add Todo</span></NavLink>
                 </nav>
 
                 <Switch>
                     <Route component={this.addTextInput} path={`/note/addTextNote`} />
-                    <Route component={this.addImgInput} path={`/note/addImgNote`} />
-                    <Route component={this.addTodoInput} path={`/note/addTodoNote`} />
+                    <Route component={this.addTextInput} path={`/note/addImgNote`} />
+                    <Route component={this.addTextInput} path={`/note/addTodoNote`} />
                 </Switch>
             </div>
         )
--- Apps/Keep/services/keep-service.js
+++ Apps/Keep/services/keep-service.js
@@ -43,11 +43,12 @@ function _createNotes() {
             type: "NoteImg",
             info: {
                 url: "https://ilion.digital/wp-content/uploads/2019/10/unnamed-1.png",
-                title: "Me playing Mi"
+                title: "Me playing Mi",
+                style: {
+                    backgroundColor: "#00d"
+                }
             },
-            style: {
-                backgroundColor: "#00d"
-            }
+
         },
         {
             id: utilService.makeId(),
@@ -116,11 +117,12 @@ function _createNotes() {
             type: "NoteImg",
             info: {
                 url: "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-                title: "Me playing Mi"
+                title: "Me playing Mi",
+                style: {
+                    backgroundColor: "#00d"
+                }
             },
-            style: {
-                backgroundColor: "#00d"
-            }
+
         }
     ]
 
