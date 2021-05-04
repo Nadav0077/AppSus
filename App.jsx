8a348665789b1ffe65ae2b09a0ab672795c97853
Nadav <nadav1410@gmail.com>
2021-05-04 11:22:20 +0300

    added a lot of css files

---
+++ App.jsx
@@ -0,0 +1,27 @@
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch, Link } = ReactRouterDOM
+import { Home } from './pages/Home.jsx'
+import { MailApp } from './pages/MailApp.jsx'
+import { NoteApp } from './pages/NoteApp.jsx'
+import { AppHeader } from './cmps/AppHeader.jsx'
+
+
+export class App extends React.Component {
+    render() {
+        return (
+            // <Book></Book>
+            <Router>
+                <AppHeader/>
+                <main>
+                    <Switch>
+                    {/* <Route component={BookEdit} path="/car/edit/:carId?" /> */}
+                        <Route component={MailApp} path="/mail" />
+                        <Route component={NoteApp} path="/note" />
+                        <Route component={Home} path="/" />
+                    </Switch>
+                </main>
+            </Router>
+            )
+    }
+}
+
